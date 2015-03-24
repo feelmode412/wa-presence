53cc68d78ba69559a92ddf59ee2ea916efe4c5dc
Qosdil A <qosdil@gmail.com>
2015-03-24 07:46:00 +0700

    A log in/out example on Home.

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -1,1 +1,14 @@
-<p>Welcome.</p>
+@if (Input::get('userId'))
+	<?php Auth::loginUsingId(Input::get('userId')) ?>
+@endif
+
+@if (Input::get('logout'))
+	<?php Auth::logout() ?>
+@endif
+
+<p>Hi, {{ Auth::check() ? Auth::user()->email.'.' : 'welcome.' }}</p>
+
+<ul>
+	<li><a href="{{ url(Auth::check() ? '?logout=1' : '?userId=2') }}">Log {{ Auth::check() ? 'out' : 'in' }}</a></li>
+	<li><a href="{{ url() }}">Home</a></li>
+</ul>
